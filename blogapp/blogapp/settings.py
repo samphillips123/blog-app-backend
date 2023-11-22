93d1d67621e1110f28e325c7c68beb1a6f27a2ee
samphillips123 <sphillips123@gmail.com>
2023-11-21 18:40:16 -0800

    chaged it back

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-
-import django_heroku
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
